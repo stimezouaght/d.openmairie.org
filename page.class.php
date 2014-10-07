8281d4d971dac55c412e39dd846239d4981db7a0
Florent Michon <fmichon@atreal.fr>
2014-10-07 13:59:08 +0000

    * Google analytics.

--- page.class.php
+++ page.class.php
@@ -207,17 +207,6 @@ $footer = "
             <p>&copy; openMairie 2004-2014 - <a href=\"http://www.openmairie.org\">openMairie.org</a>
             <small><span class=\"muted\">- Cet espace est hébergé par <a target=\"_blank\" href=\"http://www.atreal.fr/\">atReal</a></span></small></p>
         </footer>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-27539934-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 ";
         //
         printf($footer);
